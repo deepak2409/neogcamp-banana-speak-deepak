f1bc7e94e8c16a9d10ff0e7bbd4bd8cc60eb9775
Deepak Tiwari <61558056+deepak2409@users.noreply.github.com>
2021-03-09 11:02:56 +0530

    added event listener

---
+++ app.js
@@ -0,0 +1,11 @@
+// var username = prompt("Give me your name!")
+
+// var welcomeMessage = "Welcome "+username+"!"
+
+// alert(welcomeMessage)
+
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate)
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("Clicked")
+})
